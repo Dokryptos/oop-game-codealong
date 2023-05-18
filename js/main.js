26ad2cb034806f1ce8bb4b66631c6b3a41044ba7
Dok <pierredok@outlook.fr>
2023-05-18 12:49:33 +0200

    add Player class, addEventListener for moove the player on the right and left side, connect all files together

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log("js loaded");
+class Player{
+    constructor(){
+        this.positionX = 45;
+        this.positionY = 0;
+        this.width = 10;
+        this.height = 5;
+        
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        this.domELement = document.createElement("div");
+
+        this.domELement.id = 'player'
+        this.domELement.style.width = this.width + 'vw';
+        this.domELement.style.height = this.height + 'vh';
+        this.domELement.style.bottom = this.positionY + 'vh' ;
+        this.domELement.style.left = this.positionX + 'vw';
+
+
+        const playerElmId = document.getElementById('board');
+        playerElmId.appendChild(this.domELement)
+    }
+    moveLeft(){
+        this.positionX--;
+        this.domELement.style.left = this.positionX + 'vw';
+    }
+
+    moveRight(){
+        this.positionX++;
+        this.domELement.style.left = this.positionX + 'vw';
+    }
+}
+
+
+
+
+
+const player = new Player();
+
+
+// Event listeners
+document.addEventListener("keydown", (Event) =>{
+    if(Event.code === 'ArrowRight'){
+        player.moveRight();
+    } else if(Event.code === 'ArrowLeft'){
+        player.moveLeft();
+    }
+})
